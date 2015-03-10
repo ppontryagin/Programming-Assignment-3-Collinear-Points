2f5f8aa22a92b75b6925bf61053891670f87eb98
Pavel.Pontryagin <Pavel.Pontryagin@billing.ru>
2015-03-10 16:06:20 +0300

    Added Point class and tests

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,3 +1,78 @@
-public class Point {
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
 
+import java.util.Comparator;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER = null;       // YOUR DEFINITION HERE
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        
+        //check the same node
+        if ((Double.compare(that.x, this.x) == 0) && (Double.compare(that.y, this.y) == 0)) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        
+        //check vertical
+        if (Double.compare(that.x, this.x) == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+  
+        return (that.y - this.y) / (that.x - this.x);
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        
+        return x;
+        /* YOUR CODE HERE */
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,3 +1,39 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class PointTest {
+    
+    @Test
+    public void slopeIsCorrect() {
+        
+        // -45
+        Point p0 = new Point(0, 0); 
+        Point p1 = new Point(-1, 1); 
+        
+        double slope = p0.slopeTo(p1);  
+        
+        assertTrue(Double.compare(-1D, slope) == 0);
+        
+        // 45
+        p1 = new Point(1, 1);
+        slope = p0.slopeTo(p1);
+        assertTrue(Double.compare(1D, slope) == 0);
+        
+        // horizontal - positive zero
+        p1 = new Point(1, 0);
+        slope = p0.slopeTo(p1);
+        assertTrue(Double.compare(0, slope) == 0);
+
+
+        // vertical - positive infinity
+        p1 = new Point(0, 1);
+        slope = p0.slopeTo(p1);
+        assertTrue(Double.compare(Double.POSITIVE_INFINITY, slope) == 0);
+        
+        //between a point and itself) - negative infinity
+        p1 = new Point(0, 0);
+        slope = p0.slopeTo(p1);
+        assertTrue(Double.compare(Double.NEGATIVE_INFINITY, slope) == 0);
+    }
 
 }
